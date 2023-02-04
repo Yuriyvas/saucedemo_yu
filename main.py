89117147966cbc80a2678c4eeea6a374af14ae8e
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-04 15:34:09 +0500

    first 3 ignore

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    # Use a breakpoint in the code line below to debug your script
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
