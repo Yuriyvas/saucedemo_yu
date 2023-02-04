d6b52cc38485fc5c551175219c0b7d7aa0ed8137
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-04 22:49:19 +0500

    test added

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,9 +1,10 @@
 def test_check():
     c = 5
-    print('Hello world'*c)
+    print('Hello world' * c)
     assert c == 5
+
 
 def test_check_wrong():
     c = 5
-    print('Hello world'*c)
+    print('Hello world' * c)
     assert c == 6
