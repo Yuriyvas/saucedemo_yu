98de25e3d3c6546b92a3f44d2463c9b73ef6625d
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-05 16:23:12 +0500

    try black

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,10 +1,14 @@
+import pytest
+
+
 def test_check():
     c = 5
     print('Hello world' * c)
     assert c == 5
 
 
-# def test_check_wrong():
-#     c = 5
-#     print('Hello world' * c)
-#     assert c == 6
+@pytest.mark.xfail
+def test_check_wrong():
+    c = 5
+    print('Hello world' * c)
+    assert c == 6
