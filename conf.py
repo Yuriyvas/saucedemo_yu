d8a2cf7222e2e0899fe2a2f14e17b15eb1a4aaf6
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-10 23:11:06 +0500

    added screenshots to the allure report

--- conf.py
+++ conf.py
@@ -1,7 +1,3 @@
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
 URL = 'https://www.saucedemo.com/'
 LOGIN_PAGE_URL = "https://www.saucedemo.com/"
 TITLE = "Swag Labs"
