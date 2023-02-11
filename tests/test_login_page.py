57b48927d9d4276e9a3f9fa6a3468edd22f80e00
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-11 14:46:58 +0500

    added screenshots to the allure report

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -27,19 +27,19 @@ def test_login_form():
     input_user_name = driver.find_element(By.ID, 'user-name')
     input_user_name.send_keys('standard_user')
     time.sleep(5)
-    with allure.step('Вводим логин и' + ' ' + 'делаем скриншот'):
-        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-
-    input_user_password = driver.find_element(By.ID, 'password')
-    input_user_password.send_keys('secret_sauce')
-    time.sleep(5)
-    with allure.step('Вводим пароль и' + ' ' + 'делаем скриншот'):
-        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-
-    input_login_button = driver.find_element(By.ID, 'login-button')
-    input_login_button.click()
-    time.sleep(5)
-
-    with allure.step('Жмём на кнопку "LOGIN" и' + ' ' + 'делаем скриншот'):
-        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    # with allure.step('Вводим логин и' + ' ' + 'делаем скриншот'):
+    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    #
+    # input_user_password = driver.find_element(By.ID, 'password')
+    # input_user_password.send_keys('secret_sauce')
+    # time.sleep(5)
+    # with allure.step('Вводим пароль и' + ' ' + 'делаем скриншот'):
+    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    #
+    # input_login_button = driver.find_element(By.ID, 'login-button')
+    # input_login_button.click()
+    # time.sleep(5)
+    #
+    # with allure.step('Жмём на кнопку "LOGIN" и' + ' ' + 'делаем скриншот'):
+    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
     assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We are reached another site'
