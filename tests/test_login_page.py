564302b6271ee55aeb66513e67beeb73d149174c
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-11 15:04:53 +0500

    added screenshots to the allure report

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -6,10 +6,13 @@ import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.common.by import By
 
 # my first selenium test
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+# service = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service)
 driver.get(conf.URL)
 
 
@@ -27,19 +30,19 @@ def test_login_form():
     input_user_name = driver.find_element(By.ID, 'user-name')
     input_user_name.send_keys('standard_user')
     time.sleep(5)
-    # with allure.step('Вводим логин и' + ' ' + 'делаем скриншот'):
-    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-    #
-    # input_user_password = driver.find_element(By.ID, 'password')
-    # input_user_password.send_keys('secret_sauce')
-    # time.sleep(5)
-    # with allure.step('Вводим пароль и' + ' ' + 'делаем скриншот'):
-    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-    #
-    # input_login_button = driver.find_element(By.ID, 'login-button')
-    # input_login_button.click()
-    # time.sleep(5)
-    #
-    # with allure.step('Жмём на кнопку "LOGIN" и' + ' ' + 'делаем скриншот'):
-    #     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    with allure.step('Вводим логин и' + ' ' + 'делаем скриншот'):
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+
+    input_user_password = driver.find_element(By.ID, 'password')
+    input_user_password.send_keys('secret_sauce')
+    time.sleep(5)
+    with allure.step('Вводим пароль и' + ' ' + 'делаем скриншот'):
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+
+    input_login_button = driver.find_element(By.ID, 'login-button')
+    input_login_button.click()
+    time.sleep(5)
+
+    with allure.step('Жмём на кнопку "LOGIN" и' + ' ' + 'делаем скриншот'):
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
     assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We are reached another site'
