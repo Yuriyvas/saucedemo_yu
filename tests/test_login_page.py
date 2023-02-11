8aae045663d29491bf8ff86c22d8ec311cc930f2
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-11 23:33:36 +0500

    added screenshots to the allure report + Chrome is service

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -14,10 +14,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-options = Options()
-options.add_argument("start-maximized")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = WebDriver()
+
+# options = Options()
+# options.add_argument("start-maximized")
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 # driver.get("https://www.google.com")
 
 # my first selenium test
