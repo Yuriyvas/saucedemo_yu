e982fabe8604c33f18a704388003526a8c95c05a
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-02-14 17:29:31 +0500

    try POM

--- pages/page_headless.py
+++ pages/page_headless.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-options.headless = True
+# options.headless = True
+options.add_argument('-headless')
 driver = webdriver.Firefox(options=options)
 
